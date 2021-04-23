0a11fb98b5b257150d2254889298f0f14a0c7d27
tkolsrud <mfliltim@gmail.com>
2021-04-23 15:27:23 -0700

    packages/requires for basic structure

---
+++ models/Index.js
@@ -0,0 +1,28 @@
+// require mongoose
+const mongoose = require("mongoose");
+
+// connect to mongodb
+const dbUrl = "mongodb://localhost:27017/carsdb";
+
+// connect mongoose
+mongoose
+    .connect(dbUrl, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+    })
+    .then(function () {
+        console.log("MongoDB connected!");
+    })
+    .catch(function (err) {
+        console.log("MongoDB error");
+    });
+
+mongoose.connection.on("disconnected", function () {
+    console.log("MongoDB disconnected");
+});
+
+module.exports = {
+
+}
