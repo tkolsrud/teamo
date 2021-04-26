0edea5016809ff5329e786839a8bb526ed211c1f
k-kendrick12 <gkend@hotmail.com>
2021-04-26 16:31:21 -0700

    updated help page with nav

--- controllers/site.js
+++ controllers/site.js
@@ -6,5 +6,9 @@ const db = require('../models')
 router.get('/about', function (req, res) {
     res.render('site/about');
 });
+/***** Help Route *********/
+router.get('/help', function (req, res) {
+    res.render('site/help');
+});
 
 module.exports = router;
