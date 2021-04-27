a76dbaf8d06984e3a622989f60df095a34fe89b5
tkolsrud <mfliltim@gmail.com>
2021-04-27 15:27:55 -0700

    index -> show page functional

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,34 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../models')
+const bcrypt = require('bcruptjs');
+const db = require('../models');
 
 /** Register Route ***/
 
 router.get('/register', function (req, res) {
     res.render('auth/register');
 });
+
+router.post('/register', async function (req, res) {
+    try {
+        const foundUser = await db.User.findOne({ email: req.body.email });
+        if (foundUser) {
+            return res.redirect('/login');
+        }
+        const salt = await bcrypt.genSalt(10);
+        const hash = await bcrypt.hash(req.body.password, salt);
+
+        req.body.password = hash;
+        const newUser = await db.User.create(req.body);
+
+        return res.redirect('/login');
+    } catch (err) {
+        console.log(err);
+        return res.send(err);
+    }
+});
+
+
 
 
 /** Login Route ***/
--- models/User.js
+++ models/User.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema(
         email: { type: String, require: [true, "Please Provide Valid Email Address."], unique: true },
         password: { type: String, required: [true, "Please Enter A Password"], unique: true },
         username: { type: String, required: [true, "Please Enter A Username."], unique: true },
-        garage: [{ type: String }],
+        garage: [{ type: mongoose.Schema.Types.ObjectId, ref: "Car" }],
     },
     {
         timestamps: true,
