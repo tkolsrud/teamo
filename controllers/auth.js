08ae943226833e26a99fe32925a9f9760163e1dc
k-kendrick12 <gkend@hotmail.com>
2021-04-26 17:57:42 -0700

    stubbed the routes

---
+++ controllers/auth.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../models')
+
+/** Register Route ***/
+
+router.get('/register', function (req, res) {
+    res.render('auth/register');
+});
+
+
+/** Login Route ***/
+
+router.get('/login', function (req, res) {
+    res.render('auth/login');
+});
--- controllers/site.js
+++ controllers/site.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const db = require('../models')
 
+// About route - Presentational X
+// Help route - Presentational 
+// Home route - Presentational
+
 /***** About Route *********/
 router.get('/about', function (req, res) {
     res.render('site/about');
@@ -11,4 +15,28 @@ router.get('/help', function (req, res) {
     res.render('site/help');
 });
 
+/***** Home Route *********/
+router.get('/home', function (req, res) {
+    res.render('site/home');
+});
+
+/***** Index Route *********/
+router.get('/index', function (req, res) {
+    res.render('site/index');
+});
+
+/***** Collection Route *********/
+router.get('/collection', function (req, res) {
+    res.render('site/collection');
+});
+
+/***** Show Route *********/
+router.get('/show', function (req, res) {
+    res.render('site/show');
+});
+
+/***** New Car Route *********/
+router.get('/newcar', function (req, res) {
+    res.render('site/newcar');
+});
 module.exports = router;
