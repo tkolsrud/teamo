985a95b84420af252342111949247b32da685e62
tkolsrud <mfliltim@gmail.com>
2021-04-28 10:01:51 -0700

    added registration/login forms

--- controllers/auth.js
+++ controllers/auth.js
@@ -11,16 +11,17 @@ router.get('/register', function (req, res) {
 
 router.post('/register', async function (req, res) {
     try {
-        const foundUser = await db.User.findOne({ email: req.body.email });
+        const foundUser = await db.User.findOne({ email: req.body.email })
+
         if (foundUser) {
             return res.redirect('/login');
         }
+
         const salt = await bcrypt.genSalt(10);
         const hash = await bcrypt.hash(req.body.password, salt);
-
         req.body.password = hash;
-        const newUser = await db.User.create(req.body);
 
+        const newUser = await db.User.create(req.body);
         return res.redirect('/login');
     } catch (err) {
         console.log(err);
--- server.js
+++ server.js
@@ -27,19 +27,22 @@ app.use(express.static(`${__dirname}/public`));
 
 
 // // setup session middleware
-// app.use(session({
-//     store: MongoStore.create({ mongoUrl: process.env.MONGO_URI }),
-//     secret: process.env.SESSION_SECRET,
-//     resave: false,
-//     saveUninitialized: false,
-// }));
+app.use(session)({
+    store: MongoStore.create({ mongoUrl: process.env.MONGO_URI }),
+    secret: "super secret waffles",
+    resave: false,
+    saveUnitialized: false,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24
+    }
+});
 
 // // logger middleware
-// app.use(function (req, res, next) {
-//     console.log(`${req.method} - ${req.url}`);
-//     console.log(req.session);
-//     next();
-// });
+app.use(function (req, res, next) {
+    console.log(`${req.method} - ${req.url}`);
+    console.log(req.session);
+    next();
+});
 
 // // authRequired middleware
 // const authRequired = function (req, res, next) {
@@ -55,11 +58,12 @@ app.use(express.static(`${__dirname}/public`));
 app.get("/", function (req, res) {
     res.render("Welcome");
 });
+//auth controller
+app.use('/', controllers.auth);
 
 // site controller
 app.use('/', controllers.site);
 
-app.use('/', controllers.auth);
 
 
 
